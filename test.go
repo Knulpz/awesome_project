a5f29ab7cb26e35d0cfed9eef359f7847efd5869
Knulp <SuchenSonne@outlook.com>
2021-12-14 22:56:41 +0800

    gopl update

--- test.go
+++ test.go
@@ -1,16 +1,46 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hi Voyager %s", r.URL.Path[1:])
+type TreeNode struct {
+	Val int
+	    Left *TreeNode
+	    Right *TreeNode
 }
 
 func main(){
-	http.HandleFunc("/", handler)
+
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	res := [][]int{}
+	if root == nil{
+		return res
+	}
+	//借助队列实现层序上的一个接一个
+	queue := make([]*TreeNode, 0)
+	//记录一层的数据
+	tmp_arr := make([]int, 0)
+	queue = append(queue, root)
+	//外层循环代表每层
+	for len(queue) > 0{
+		//内层循环遍历该层的每个节点
+		for i := 0; i < len(queue); i++{
+			//每次循环出队首元素（但是去世之前留下了自己的孩子）
+			root = queue[0]
+			queue = queue[1:]
+			//先进左孩子
+			if root.Left != nil{
+				queue = append(queue,root.Left)
+			}
+			//再进右孩子
+			if root.Right != nil{
+				queue = append(queue,root.Right)
+			}
+			tmp_arr = append(tmp_arr,root.Val)
+		}
+		res = append(res, tmp_arr)
+		tmp_arr = make([]int, 0)
+	}
+	return res
 }
 
 
